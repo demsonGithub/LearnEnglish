4b3bb77ca231b9f9ae5eaa74fcceb236842f79a1
wangzhixiong <demkin@163.com>
2022-09-18 00:04:51 +0800

    初始化

--- src/FrontEnd/LearnEnglish_AdminUI/vite.config.ts
+++ src/FrontEnd/LearnEnglish_AdminUI/vite.config.ts
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/styles/variables.scss";',
+      },
+    },
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: [
+            'Android 4.1',
+            'iOS 7.1',
+            'Chrome > 31',
+            'ff > 31',
+            'ie >= 8',
+            '> 1%',
+          ],
+          grid: true,
+        }),
+        {
+          // 去除警告: [WARNING] "@charset" must be the first rule in the file
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: atRule => {
+              if (atRule.name === 'charset') {
+                atRule.remove()
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
 })
